8fea18a5b5315006221be24bea2f4b9c1da2fa00
Ivan Shychynov <i.shychynov@fdcl.nl>
2026-07-13 13:24:30 +0200

    2017 16+17

--- 2017/16.h
+++ 2017/16.h
@@ -12,29 +12,91 @@
 #include <unordered_set>
 #include "utils.h"
 
-//TODO: put a small description of the task
+// "dancing" with letters
+
+using Program = std::vector<std::function<void(std::string&)>>;
 struct Task
 {
+	static constexpr std::string order(int x) {
+		std::string order;
+		for (int i=0; i<x; i++) {
+			order += ('a' + i);
+		}
+		return order;
+	}
+
+	static inline Program prog;
 	static void run() {
 
-///TODO: CHANGE THE DAMN FILE NAME BEFORE YOU FORGET
-
-		std::ifstream inputStream("20XX/input/XX.txt");
+		std::ifstream inputStream("2017/16.txt");
 
 		if (!inputStream.is_open()) {
 			std::cerr << "Failed to open file!\n";
 			return;
 		}
 
-		std::string line;
-		while (std::getline(inputStream, line))
+		std::string str = order(16);
+		char op;
+		while (inputStream >> op)
 		{
-
+			int arg1, arg2;
+			char arg1Ch, arg2Ch;
+			if (op == 's') {
+				inputStream >> arg1;
+				inputStream.ignore();
+				prog.emplace_back([arg1](std::string& str) {
+					std::string rest = str.substr(str.length() - arg1, arg1);
+					str.erase(str.end() - arg1, str.end());
+					str.insert(0, rest);
+				});
+			}
+			else if (op == 'x') {
+				inputStream >> arg1;
+				inputStream.ignore();
+				inputStream >> arg2;
+				inputStream.ignore();
+				prog.emplace_back([arg1, arg2](std::string& str) {
+					std::swap(str[arg1], str[arg2]);
+				});
+			}
+			else if (op == 'p') {
+				inputStream >> arg1Ch;
+				inputStream.ignore();
+				inputStream >> arg2Ch;
+				inputStream.ignore();
+				prog.emplace_back([arg1Ch, arg2Ch](std::string& str) {
+					std::swap(str[str.find(arg1Ch)], str[str.find(arg2Ch)]);
+				});
+			}
 		}
 		inputStream.close();
+		for (auto& line : prog) {
+			line(str);
+		}
+		std::cout << str << std::endl;
 	}
 	static void runPart2() {
-
+		const int iterations = 1000000000;
+		std::string str = order(16);
+		int loop = -1;
+		for (int i=0; i<iterations; i++) {
+			for (auto& line : prog) {
+				line(str);
+			}
+			if (str == order(16)) {
+				loop = i + 1;
+				std::cout << "loop found after " << loop << " iterations" << std::endl;
+				break;
+			}
+		}
+		if (loop != -1) {
+			for (int i=0; i < iterations % loop; i++) {
+				for (auto& line : prog) {
+					line(str);
+				}
+			}
+		}
+		std::cout << str << std::endl;
 	}
 };
 
--- 2017/17.h
+++ 2017/17.h
@@ -12,29 +12,41 @@
 #include <unordered_set>
 #include "utils.h"
 
-//TODO: put a small description of the task
+// spinning and inserting values
 struct Task
 {
 	static void run() {
-
-///TODO: CHANGE THE DAMN FILE NAME BEFORE YOU FORGET
-
-		std::ifstream inputStream("20XX/input/XX.txt");
-
-		if (!inputStream.is_open()) {
-			std::cerr << "Failed to open file!\n";
-			return;
+		const int input = 329;
+		const int size = 2017;
+		std::vector<int> arr{0};
+		int it = 0;
+		for (int next=1; next<=size; next++) {
+			it += input;
+			if (it >= next) {
+				it %= next;
+			}
+			arr.insert(arr.begin() + it++ + 1, next);
 		}
-
-		std::string line;
-		while (std::getline(inputStream, line))
-		{
-
-		}
-		inputStream.close();
+		std::cout << arr[it+1] << std::endl;
+		std::cout << arr[1] << std::endl;
 	}
 	static void runPart2() {
-
+		const int input = 329;
+		const int size = 50000000;
+		std::vector<int> arr{0};
+		int it = 0;
+		int res = 0;
+		for (int next=1; next<=size; next++) {
+			it += input;
+			if (it >= next) {
+				it %= next;
+			}
+			if (it == 0) {
+				res = next;
+			}
+			it++;
+		}
+		std::cout << res << std::endl;
 	}
 };
 
