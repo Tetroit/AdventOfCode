54d914fa6e16d08fa24ced5022b4bafcdc6dfc66
Ivan Shychynov <i.shychynov@fdcl.nl>
2026-07-13 13:24:14 +0200

    2017 16+17

---
+++ 2017/17.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <regex>
+#include <unordered_map>
+#include <unordered_set>
+#include "utils.h"
+
+//TODO: put a small description of the task
+struct Task
+{
+	static void run() {
+
+///TODO: CHANGE THE DAMN FILE NAME BEFORE YOU FORGET
+
+		std::ifstream inputStream("20XX/input/XX.txt");
+
+		if (!inputStream.is_open()) {
+			std::cerr << "Failed to open file!\n";
+			return;
+		}
+
+		std::string line;
+		while (std::getline(inputStream, line))
+		{
+
+		}
+		inputStream.close();
+	}
+	static void runPart2() {
+
+	}
+};
+
+//-------------- NOTES AREA ----------------
+/*
+
+*/
